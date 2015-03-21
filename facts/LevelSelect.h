b5e00fb3eae1149bcdbe18cf40cb0d4d753fe8f6
Giau Ngo <giaunv@gmail.com>
2015-03-21 12:30:59 +0700

    Level Select

---
+++ facts/LevelSelect.h
@@ -0,0 +1,16 @@
+//
+//  LevelSelect.h
+//  facts
+//
+//  Created by giaunv on 3/21/15.
+//  Copyright (c) 2015 366. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface LevelSelect : SKScene<UITableViewDataSource, UITableViewDelegate>
+@property (nonatomic, retain) UIButton* backButton;
+@property (retain, nonatomic) IBOutlet UITableView* tableView;
+@property (strong, nonatomic) NSArray* levelsArray;
+@property (strong, nonatomic) NSArray* levelsDescriptionArray;
+@end
